bf2d1151ac1a30ea942f8d264b3ba034aa10c701
Bdmnss <lukabadali123@gmail.com>
2024-07-20 23:14:02 +0400

    add burger menu with logic

--- js/header.js
+++ js/header.js
@@ -1,4 +1,9 @@
 const header = document.querySelector("header");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+const burgerIcon = document.querySelector(".burger-icon");
+const burgerMenu = document.querySelector(".burger-menu");
+isMenuOpen = false;
 let lastScrollTop = 0;
 
 window.addEventListener("scroll", function () {
@@ -15,4 +20,19 @@ window.addEventListener("scroll", function () {
   }
 
   lastScrollTop = scrollTop;
+});
+
+burgerIcon.addEventListener("click", function () {
+  main.classList.toggle("isMenuOpen");
+  footer.classList.toggle("isMenuOpen");
+  isMenuOpen = !isMenuOpen;
+
+  if (isMenuOpen) {
+    burgerMenu.classList.remove("isMenuOpen");
+    burgerIcon.classList.add("closeIcon");
+    header.style.backgroundColor = "#f9fafa";
+  } else {
+    burgerMenu.classList.add("isMenuOpen");
+    burgerIcon.classList.remove("closeIcon");
+  }
 });
